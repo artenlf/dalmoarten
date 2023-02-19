1c47d3175e63da9b066c4a0032017a857d31cacb
artenlf <felipe.arten@gmail.com>
2023-02-19 20:49:54 -0300

    feat/styles: home structure added styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/Logo.png"
 
 export function Header() {
   return (
-    <div
+    <header
       className="bg-slate-900 w-full h-32 flex items-center justify-between px-8"
     >
       <Image
@@ -13,31 +13,31 @@ export function Header() {
         height={96}
         alt=""
       />
-      <nav className="text-gray-100 text-lg flex gap-8 mr-[-50rem]">
+      <nav className="text-gray-100 text-lg flex gap-8">
         <Link
           href={'/'}
-          className="hover:text-gold-700 transition ease-in-out delay-200 
+          className="hover:text-gold-700 transition ease-in-out delay-140 
           hover:-translate-y-1 hover:scale-110 hover:underline"
         >
           Home
         </Link>
         <Link
           href={'/about'}
-          className="hover:text-gold-700 transition ease-in-out delay-200 
+          className="hover:text-gold-700 transition ease-in-out delay-140 
           hover:-translate-y-1 hover:scale-110 hover:underline"
         >
           Sobre
         </Link>
         <Link
           href={'/services'}
-          className="hover:text-gold-700 transition ease-in-out delay-200 
+          className="hover:text-gold-700 transition ease-in-out delay-140 
           hover:-translate-y-1 hover:scale-110 hover:underline"
         >
           Serviços
         </Link>
         <Link
           href={'/contact'}
-          className="hover:text-gold-700 transition ease-in-out delay-200 
+          className="hover:text-gold-700 transition ease-in-out delay-140 
           hover:-translate-y-1 hover:scale-110 hover:underline"
         >
           Contato
@@ -46,8 +46,8 @@ export function Header() {
       <button
         type="button"
         className="bg-gray-100 text-slate-900 text-lg font-semibold w-40 h-14
-         hover:text-gold-700 transition ease-in-out delay-200 
-         hover:-translate-y-1 hover:scale-110 hover:bg-slate-100 duration-300"
+         hover:text-gold-700 transition ease-in-out delay-140 
+         hover:-translate-y-1 hover:scale-110 hover:bg-slate-100 duration-200"
       >
         <a
           href="https://app.acessorias.com/?login"
@@ -57,6 +57,6 @@ export function Header() {
           Login
         </a>
       </button>
-    </div>
+    </header>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <div className="bg-gray-500 h-[20rem] flex items-center px-32">
+      <h1
+        className="text-3xl w-[41rem] font-bold text-slate-900">
+        Contabilidade descomplicada e tranquilidade para o seu negócio crescer!
+      </h1>
+    </div>
   )
 }
