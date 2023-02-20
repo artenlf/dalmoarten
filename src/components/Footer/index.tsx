93a62c5296eb1bcce24110ca12bceca5523d1858
artenlf <felipe.arten@gmail.com>
2023-02-20 16:52:37 -0300

    feat: footer structure

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import Logo from "../../assets/Logo.png"
+import ProfilePicture from "../../assets/profile.jpg"
 
 
 export function Footer() {
@@ -8,10 +9,13 @@ export function Footer() {
     <footer
       className="bg-slate-900 w-full flex flex-col gap-8 px-8 py-8"
     >
-      <div className="flex gap-32 items-center">
+      <div
+        className="flex gap-32 items-center justify-between pb-8 
+      border-b-[1px] border-b-gray-500"
+      >
         <Image src={Logo} alt='' width={120} />
-        <div className="max-w-[35rem] text-gray-100 font-bold">
-          <span>Rua Barão de Jaguara, 1481 - Ed. Cruz Alta- 10º andar - Cj. 102/104. Centro, Campinas/SP.</span>
+        <div className="max-w-[35rem] text-gray-100 font-bold space-y-2">
+          <span>Rua Barão de Jaguara, 1481 - Ed. Cruz Alta - 10º andar - Cj. 102/104 Centro, Campinas/SP</span>
           <p>CEP: <span>13015-002</span></p>
           <p>Telefone: <span>(19) 3237-1983 / (19) 3237-1974</span></p>
         </div>
@@ -60,12 +64,21 @@ export function Footer() {
             Área do Cliente
           </a>
         </nav>
+        <div className="text-gray-100 grid grid-cols-2 gap-1">
+          <Image
+            src={ProfilePicture}
+            width={120} alt=""
+            className="row-span-3 rounded-full"
+          />
+          <p className="font-bold mb-8">Responsável Técnico</p>
+          <span>Dalmo Otacílio Arten</span>
+          <p>CRC SP-140299/O-5</p>
+        </div>
       </div>
-
       <p
-        className="text-gray-100 place-self-center"
+        className="text-gray-100 text-sm mx-auto my-[-1.75rem]"
       >
-        Dalmo Arten Contabilidade. {new Date().getFullYear()}.
+        {new Date().getFullYear()} &#xAE; Dalmo Arten Contabilidade.
         Desenvolvido com &#128151; por artenlf.
       </p>
     </footer >
