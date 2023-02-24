9cfbb3860f73c6146914de478de9c602e0126a1f
artenlf <felipe.arten@gmail.com>
2023-02-24 18:01:18 -0300

    styles: added custom animation to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,21 @@ module.exports = {
       fontFamily: {
         'sans': 'Montserrat, sans-serif',
       },
+      keyframes: {
+        slideDown:{
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        slideUp:{
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown 0.3s cubic-bezier(0.87, 0, 0.13, 1) forwards',
+        slideUp: 'slideUp 0.3s cubic-bezier(0.87, 0, 0.13, 1) forwards'
+
+      }
     },
     
   },
