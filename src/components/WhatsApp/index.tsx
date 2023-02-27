e7caeab2fc98476fff0f89ca2e8f14baf13fcdc4
artenlf <felipe.arten@gmail.com>
2023-02-26 21:31:20 -0300

    fix: position of whatsapp icon

--- src/components/WhatsApp/index.tsx
+++ src/components/WhatsApp/index.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export function WhatsAppButton() {
   return (
     <a
-      className='bg-green-500 text-white py-2 px-3 rounded-full fixed right-2 bottom-2
+      className='bg-green-500 text-white py-2 px-3 rounded-full fixed right-8 bottom-8
       hover:-translate-1 hover:scale-110'
       href=""
     >
