f9e463b48206161b9fd10146220f1b4378cf04c4
artenlf <felipe.arten@gmail.com>
2023-02-28 19:28:56 -0300

    feat: new hero background and text colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,26 +8,26 @@ import BannerImage from "../assets/hero/2.svg";
 export default function Home() {
   return (
     <main
-      className="bg-[url('../assets/hero/2.svg')]
-    bg-center bg-no-repeat bg-fixed"
+      className="bg-[url('../assets/hero/3.svg')]
+    bg-cover bg-no-repeat bg-fixed"
     >
       <section
         className=" flex items-center justify-evenly 
-        bg-slate-300 bg-opacity-95 h-[calc(100vh-8rem)]
+        bg-slate-300 bg-opacity-10 h-[calc(100vh-8rem)]
       "
       >
-        <div className="flex items-center justify-between py-8 px-32
-         bg-slate-300 bg-opacity-60"
+        <div className="flex items-center justify-between py-8 px-32"
+        //  bg-slate-300 bg-opacity-60"
         >
           <div
             className="flex flex-col gap-8"
           >
             <h1
-              className="text-4xl font-bold text-slate-900">
+              className="text-4xl font-bold text-slate-100">
               Contabilidade descomplicada e tranquilidade para o seu negócio crescer!
             </h1>
             <span
-              className="text-gray-600 text-justify max-w-[32.5rem] ml-2"
+              className="text-slate-200 text-justify max-w-[32.5rem] ml-2"
             >
               Oferecemos planos de serviços especializados para simplificar a
               sua rotina e poupar tempo e dinheiro. No digital, ou no presencial,
@@ -36,9 +36,9 @@ export default function Home() {
               e pagando o mínimo de impostos?
             </span>
             <button
-              className="bg-slate-900 text-gray-100 font-bold py-5 w-48 rounded-full 
+              className="bg-slate-400 text-slate-900 font-bold py-5 w-48 rounded-full 
           hover:text-gold-700 transition ease-in-out delay-140 
-         hover:-translate-1 hover:scale-110 hover:bg-slate-700 duration-200"
+         hover:-translate-1 hover:scale-110 hover:bg-slate-500 duration-200"
             >
               Saiba Mais
             </button>
@@ -47,6 +47,7 @@ export default function Home() {
             src={BannerImage}
             height={400}
             alt=""
+            className="opacity-0"
           />
         </div>
       </section >
@@ -247,7 +248,7 @@ export default function Home() {
         className=" bg-slate-300 bg-opacity-95 "
       >
         <div className="flex flex-col gap-4 items-center justify-between p-8
-         bg-slate-300 bg-opacity-60">
+         bg-slate-300 bg-opacity-20">
           <h2
             className="font-bold text-3xl text-slate-900 mx-auto max-w-xl"
           >
