db603d7dea1e2fc84cab9b427b19cd6b58af5e52
artenlf <felipe.arten@gmail.com>
2023-02-28 15:27:22 -0300

    fix/styles: added background blur in services section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -244,56 +244,64 @@ export default function Home() {
         <p className="text-sm text-gray-300 mt-8">Não encontrou a sua atividade? Envie sua mensagem agora mesmo para que um de nossos especialistas possa te orientar, sem custo algum.</p>
       </section>
       <section
-        className=" bg-slate-300 bg-opacity-95 flex flex-col p-8 gap-4"
+        className=" bg-slate-300 bg-opacity-95 "
       >
-        <h2
-          className="font-bold text-3xl text-slate-900 mx-auto max-w-xl"
-        >
-          Soluções inteligentes e eficientes
-        </h2>
-        <span className="mx-auto text-[1.125rem]">
-          De empresa individual à indústria, atendimento personalizado para:
-        </span>
-        <div
-          className="grid grid-cols-2 gap-2 gap-x-16 mx-auto mt-4 pl-12"
-        >
-          <p
-            className="flex items-center gap-2"
+        <div className="flex flex-col gap-4 items-center justify-between p-8
+         bg-slate-300 bg-opacity-60">
+          <h2
+            className="font-bold text-3xl text-slate-900 mx-auto max-w-xl"
           >
-            <Buildings size={16} color="#00d1a7" weight="fill" />
-            Abertura, alteração e encerramento
-          </p>
-          <p className="flex items-center gap-2">
-            <Person size={16} color="#6900d1" weight="fill" />
-            Departamento Pessoal
-          </p>
-          <p
-            className="flex items-center gap-2"
+            Soluções inteligentes e eficientes
+          </h2>
+          <span className="mx-auto text-[1.125rem]">
+            De empresa individual à indústria, atendimento personalizado para:
+          </span>
+          <div
+            className="grid grid-cols-2 gap-2 gap-x-16 mx-auto mt-4 pl-12"
           >
-            <Receipt size={16} color="#d10054" weight="fill" />
-            Departamento Fiscal
-          </p>
-          <p className="flex items-center gap-2"
-          >
-            <Calculator size={16} color="#0050d1" weight="fill" />
-            Contabilidade e Obrigações
-          </p>
-          <button
-            className="bg-slate-900 text-gray-100 w-64 h-10 mt-8 mx-auto 
+            <p
+              className="flex items-center gap-2"
+            >
+              <Buildings size={16} color="#00d1a7" weight="fill" />
+              Abertura, alteração e encerramento
+            </p>
+            <p className="flex items-center gap-2">
+              <Person size={16} color="#6900d1" weight="fill" />
+              Departamento Pessoal
+            </p>
+            <p
+              className="flex items-center gap-2"
+            >
+              <Receipt size={16} color="#d10054" weight="fill" />
+              Departamento Fiscal
+            </p>
+            <p className="flex items-center gap-2"
+            >
+              <Calculator size={16} color="#0050d1" weight="fill" />
+              Contabilidade e Obrigações
+            </p>
+            <button
+              className="bg-slate-900 text-gray-100 w-64 h-10 mt-8 mx-auto 
             col-span-2 rounded-full
             hover:text-gold-700 transition ease-in-out delay-140 
          hover:-translate-y-1 hover:scale-110 hover:bg-slate-700 duration-200"
-          >
-            <Link href={"/services"}>
-              Ver mais serviços
-            </Link>
-          </button>
+            >
+              <Link href={"/services"}>
+                Ver mais serviços
+              </Link>
+            </button>
+          </div>
         </div>
       </section>
       <section
-        className="bg-slate-200 py-4 px-32 flex flex-col gap-2 justify-evenly items-center"
+        className="bg-slate-200 py-4 px-32 
+        flex flex-col gap-2 justify-evenly items-center"
       >
-        <h2 className="font-bold text-2xl text-slate-900">Confira os nossos Planos</h2>
+        <h2
+          className="font-bold text-2xl text-slate-900"
+        >
+          Confira os nossos Planos
+        </h2>
         <span>Na medida certa para o seu negócio</span>
         <div className="grid grid-cols-3 gap-12 my-8">
           <div className="flex flex-col gap-2 items-center max-w-sm p-6 bg-slate-100 rounded-lg">
